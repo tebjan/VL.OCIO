733aa5aac182de8fc99528219fd18da24a8a5d9d
tebjan <tebjan@gmail.com>
2024-12-20 14:32:37 +0100

    WIP: Project setup

---
+++ src/OCIOColorSpacesDynamicEnum.cs
@@ -0,0 +1,82 @@
+// Steps to implement your own enum based on this template:
+// 1) Rename "DynamicEnumTemplate" to what your enum should be named
+// 2) Rename "DynamicEnumTemplateDefinition" accordingly
+// 3) Implement the definitions GetEntries() 
+// 
+// For more details regarding the template, see:
+// https://thegraybook.vvvv.org/reference/extending/writing-nodes.html#dynamic-enums
+
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using VL.Core.CompilerServices;
+using VL.Lib.Collections;
+
+namespace VL.OCIO;
+
+[Serializable]
+public class OCIODisplayColorSpaceEnum : DynamicEnumBase<OCIODisplayColorSpaceEnum, OCIODisplayColorSpaceEnumDefinition>
+{
+    public OCIODisplayColorSpaceEnum(string value) : base(value)
+    {
+    }
+
+    [CreateDefault]
+    public static OCIODisplayColorSpaceEnum CreateDefault()
+    {
+        return CreateDefaultBase();
+    }
+}
+
+public class OCIODisplayColorSpaceEnumDefinition : DynamicEnumDefinitionBase<OCIODisplayColorSpaceEnumDefinition>
+{
+    Dictionary<string, object> entries = new Dictionary<string, object>();
+    Subject<object> trigger = new Subject<object>(); //Really just used as a trigger, the "object" is ignored
+
+    [CreateDefault]
+    public static OCIODisplayColorSpaceEnumDefinition CreateDefault()
+    {
+        return Instance;
+    }
+
+    /// <summary>
+    /// Adds an entry to the enum that can optionally have an object associated as its tag
+    /// </summary>
+    /// <param name="name">Name of the entry to add</param>
+    /// <param name="tag">Optional: Object associated to the enum entry</param>
+    public void AddEntry(string name, object? tag = null)
+    {
+        entries[name] = tag;
+        trigger.OnNext("");
+    }
+
+    /// <summary>
+    /// Removes the given entry from the enum
+    /// </summary>
+    /// <param name="name">Name of the entry to remove</param>
+    public void RemoveEntry(string name)
+    {
+        entries.Remove(name);
+        trigger.OnNext("");
+    }
+
+    /// <summary>
+    /// Removes all entries from the enum
+    /// </summary>
+    public void ClearEntries()
+    {
+        entries.Clear();
+        trigger.OnNext("");
+    }
+
+    protected override IReadOnlyDictionary<string, object> GetEntries()
+    {
+        return entries;
+    }
+
+    protected override IObservable<object> GetEntriesChangedObservable()
+    {
+        return trigger;
+    }
+
+    protected override bool AutoSortAlphabetically => true;
+}
---
+++ src/OCIOConfigUtils.cs
@@ -0,0 +1,52 @@
+﻿using OCIOSharpCLI;
+
+namespace VL.OCIO;
+public class GPUResources
+{
+    public string Shader;
+    public UniformInfo[] Uniforms;
+    public TextureInfo[] Textures;
+    public Texture3DInfo[] Textures3D;
+}
+
+public static class OCIOConfigUtils
+{
+    public static void LoadOCIOConfig(string configPath)
+    {
+        var config = new OCIOConfig();
+        config.LoadConfig(configPath);
+
+        var displays = config.GetDisplays();
+
+        // Create the color space enum
+        var def = OCIODisplayColorSpaceEnumDefinition.Instance;
+
+        // Add the color spaces to the enum
+        foreach (var display in displays)
+        {
+            foreach (var view in display.Views)
+            {
+                var tag = new Tuple<OCIOConfig, string>(config, display.Name);
+                def.AddEntry(view, tag);
+            }
+        }
+    }
+
+    public static GPUResources GetGPUResources(OCIODisplayColorSpaceEnum ocioColorSpaceEnum, string inputColorspace)
+    {
+        var tag = ocioColorSpaceEnum.Tag as Tuple<OCIOConfig, string>;
+        var config = tag.Item1;
+        var display = tag.Item2;
+        var view = ocioColorSpaceEnum.Value;
+
+        config.CreateProcessor("scene_linear", display, view);
+
+        return new GPUResources
+        {
+            Shader = config.GetHLSLShader(),
+            Uniforms = config.GetUniforms(),
+            Textures = config.GetTextures(),
+            Textures3D = config.Get3DTextures()
+        };
+    }
+}
